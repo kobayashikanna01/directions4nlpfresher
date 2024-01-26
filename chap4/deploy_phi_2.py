9a83d670fc80aef56e5737eaed55fba474e5f6f9
Mingxu_Tao <109057451+kobayashikanna01@users.noreply.github.com>
2024-01-26 22:57:32 +0800

    Update deploy_phi_2.py

--- chap4/deploy_phi_2.py
+++ chap4/deploy_phi_2.py
@@ -9,6 +9,15 @@ tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2", trust_remote_code=T
 # model = AutoModelForCausalLM.from_pretrained("/path/to/save/phi_2", torch_dtype="auto", trust_remote_code=True)
 # tokenizer = AutoTokenizer.from_pretrained("/path/to/save/phi_2", trust_remote_code=True)
 
+# 如果你需要将模型部署到多张GPU上，请按照如下方式：
+# pip install accelerate
+# 启动python时指定显卡编号，例如在命令行输入CUDA_VISIBLE_DEVICES="0,1,2,3" python
+# 进入python之后：
+# >>> model = AutoModelForCausalLM.from_pretrained("microsoft/phi-2", torch_dtype="auto", trust_remote_code=True, device_map="auto")
+
+# 如果显存不够，可以加载bf16编码的模型，几乎不会影响效果，还能节约显存、加快推理速度
+# >>> model = AutoModelForCausalLM.from_pretrained("microsoft/phi-2", torch_dtype=torch.bfloat16, trust_remote_code=True, device_map="auto")
+
 inputs = tokenizer('''def print_prime(n):
    """
    Print all primes between 1 and n
